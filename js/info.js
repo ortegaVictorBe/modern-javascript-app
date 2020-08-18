1ca066860fb0a7d494dd8e65188d1a947f8790c0
Ortega <ortega.victor@gmail.com>
2020-08-18 11:25:08 +0200

    Add picture of city

--- js/info.js
+++ js/info.js
@@ -4,53 +4,51 @@
 
 var cityName = localStorage.getItem("name_localCity");
 const URL_DAY_WEATHER = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e4dfa41e22f93aa72d2e80838a3bb930`
-
 var cityWeatherInfo;
 var cityWeatherInfoDaily;
+var cityPicture;
 var dateNow = new Date();
 
 const btnChangCity = document.getElementById("btnChangeCity")
+const btnCompareCities = document.getElementById("btnCompareCities")
+const cityImageContainer = document.getElementById("cityImage");
 
 //  -------------------------------------------------------
 //  Functions
 //  -------------------------------------------------------
 
-//getWeatherData - 
+//getWeatherDataPerDay - Function : Get data info of one day
 const getWeatherDataPerDay = async () => {
-
     const res = await fetch(URL_DAY_WEATHER);
     cityWeatherInfo = await res.json();
-    console.log(cityWeatherInfo)
+    paintWeatherGeneralInfo();
+
+    //Getting Weather Daily
+    getWeatherDataDaily();
+
+    //gatting pictyre
+    getCityPicture()
 
 }
 
+// getWeatherDataDaily - function : Get from the API the daily weather
 const getWeatherDataDaily = async () => {
-    const URL_DAILY_WEATHER = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherInfo.coord.lat}&lon=${cityWeatherInfo.coord.lon}&units=metric&
-    exclude=hourly,minutely&appid=e4dfa41e22f93aa72d2e80838a3bb930`;
+    const URL_DAILY_WEATHER = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherInfo.coord.lat}&lon=${cityWeatherInfo.coord.lon}&units=metric&exclude=hourly,minutely&appid=e4dfa41e22f93aa72d2e80838a3bb930`;
 
     const resDaily = await fetch(URL_DAILY_WEATHER);
     cityWeatherInfoDaily = await resDaily.json();
-    console.log(cityWeatherInfo)
+    paintWeatherDailyInfo();
+    console.log(cityWeatherInfoDaily)
 }
 
-const getWeather = () => {
-    getWeatherDataPerDay();
-    // getWeatherDataDaily();
-    // console.log(cityWeatherInfo);
-    // console.log(cityWeatherInfoDaily);
-
-    // paintData();
+const getCityPicture = async () => {
+    const res = await fetch(`https://api.unsplash.com/photos/random?client_id=KnqxdhP9gviZnH7O9L4xG4KVbrGZHHeooFPfFMTDctg&query=%27${cityName}%27%20SKY`);
+    cityPicture = await res.json();
+    showCityPicture();
 }
 
-//paintData - function : Write info in the browser
-const paintData = () => {
-    //Genral Section
-    paintGeneral();
-    //Daily
-    paintDaily();
-}
-
-const paintGeneral = () => {
+// paintWeatherGeneralInfo - function : paint in the html page the weather of the day
+const paintWeatherGeneralInfo = () => {
     //console.log(cityWeatherInfo.base);
     let nameCity = cityWeatherInfo.name
     let date = dateNow.toDateString();
@@ -78,51 +76,74 @@ const paintGeneral = () => {
     document.getElementById("descInfo").appendChild(descResume);
 }
 
-const paintDaily = () => {
+// paintWeatherDailyInfo - function : paint in the html page the daily weather (5 days)
+const paintWeatherDailyInfo = () => {
 
     let dailyArray = cityWeatherInfoDaily.daily;
     let detailTable = document.getElementById("detailTable")
     let nameDays = ["Mon", "Sun", "Tue", "Wen", "Thu", "Fri", "Sat"];
-    // let day = new Date(dateNow);
-    for (let i = 1; i <= dailyArray.length; i++) {
-        let day = new Date(dateNow);
-        day.setDate(day.getDate() + i);
-        // day = new Date(dateNow.setDate(dateNow.getDate() + index));
 
-        // let iconImg = `https://openweathermap.org/img/wn/${dailyArray[i].weather[0].icon}@2x.png}`
+    dailyArray.forEach((element, index) => {
+        let day = new Date(dateNow);
+        day.setDate(day.getDate() + index);
+
+        // let iconImg = `https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png}`
         // <img src="http://openweathermap.org/img/wn/02d@2x.png" class="max-width: 50%;" >
         // <img src="http://openweathermap.org/img/wn/${iconImg}@2x.png">
-
-        detailTable.innerHTML += `<tr class="table-warning">                               
+        if (index >= 1) {
+            detailTable.innerHTML += `<tr class="table-warning">                               
             <td>${nameDays[day.getDay()]}</td>
-            <td>${dailyArray[i].temp.min}°c</td>
-            <td>${dailyArray[i].temp.max}°c</td>
+            <td>${element.temp.min}°c</td>
+            <td>${element.temp.max}°c</td>
           </tr> `
+        }
 
-    }
-
-    // dailyArray.forEach((element, index) => {
-    //     let day = new Date(dateNow);
-    //     day.setDate(day.getDate() + index);
-    //     // day = new Date(dateNow.setDate(dateNow.getDate() + index));
-
-    //     let iconImg = `https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png}`
-    //     // <img src="http://openweathermap.org/img/wn/02d@2x.png" class="max-width: 50%;" >
-    //     // <img src="http://openweathermap.org/img/wn/${iconImg}@2x.png">
-
-    //     detailTable.innerHTML += `<tr class="table-warning">                               
-    //         <td>${nameDays[day.getDay()]}</td>
-    //         <td>${element.temp.min}°c</td>
-    //         <td>${element.temp.max}°c</td>
-    //       </tr> `
-
-    // });
-
-    console.table(dailyArray)
-
+    });
 }
 
+// getWeather - function : Get the weather per day and daily
+const getWeather = () => {
+    getWeatherDataPerDay();
+}
+
+// setLocalStorage - function : Set the Values of Local Storage
+const setLocalStorage = (StorageValue) => {
+    if (StorageValue == "") {
+        localStorage.setItem("id_localCity", StorageValue);
+        localStorage.setItem("name_localCity", StorageValue);
+        localStorage.setItem("lat_localCity", StorageValue);
+        localStorage.setItem("lon_localCity", StorageValue);
+    }
+}
+
+// showCitiPicture - function : shows picture of city
+const showCityPicture = () => {
+
+    let imageCityCard = document.createElement("div");
+    let urlImage = cityPicture.urls.regular;
+    console.log(urlImage);
+
+    imageCityCard.innerHTML = `
+        <div class="card border-dark  bg-info mb-3" style="max-width: 20rem;">
+      
+      <div class="card-body">
+        <h4 class="card-title">${cityName}</h4>
+        <img style="height: 200px; width: 100%; display: block;" src="${urlImage}" alt="Card image">
+        
+      </div>
+    </div>`
+    cityImageContainer.appendChild(imageCityCard);
+}
+
+
+// changeCity - function : Change the city origen
 const changeCity = () => {
+    setLocalStorage("");
+    window.open("index.html", "_self")
+}
+
+// compareCities - function : compare 2 citie's weather
+const compareCities = () => {
 
 }
 //  -------------------------------------------------------
@@ -130,6 +151,7 @@ const changeCity = () => {
 //  -------------------------------------------------------
 
 btnChangCity.addEventListener('click', changeCity);
+btnCompareCities.addEventListener('click', compareCities);
 
 //  -------------------------------------------------------
 //  Execution Secuence.
